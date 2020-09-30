3662aaf9f21e4b928f632512477a8011bffdae07
mahidulmoon <mahidulmoon@gmail.com>
2020-09-30 15:04:44 +0600

    variable declaration success

--- src/first/hello.go
+++ src/first/hello.go
@@ -2,5 +2,15 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("hello mahidulmoon")
+	// fmt.Println("hello mahidulmoon")
+	var moon int = 34
+	mahidul := 3
+	fmt.Println(moon)
+	fmt.Println(mahidul)
+
+	a,b,c := 2,3,4
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	
 }
