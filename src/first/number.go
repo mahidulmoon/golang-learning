b97713809a9780a8032e879548662b73abecb444
mahidulmoon <mahidulmoon@gmail.com>
2020-09-30 15:11:09 +0600

    number system success

---
+++ src/first/number.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main(){
+	first := 3
+	last := 5
+
+	fmt.Println("Add:",first+last)
+	fmt.Println("minus:",first-last)
+	fmt.Println("mul:",first*last)
+	fmt.Println("div:",first/last)
+	fmt.Println("mod:",first%last)
+}
