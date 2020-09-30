96f78b33a37808a97efe2398a3077a9195bb8d4f
mahidulmoon <mahidulmoon@gmail.com>
2020-09-30 19:19:20 +0600

    anonymous function handler

--- src/restapi/httpd/handler/ping_get.go
+++ src/restapi/httpd/handler/ping_get.go
@@ -2,8 +2,10 @@ package handler
 import "github.com/gin-gonic/gin"
 
 
-func PingGet(c *gin.Context){
-	c.JSON(200, gin.H{
-		"hello": "found me",
-	})
+func PingGet() gin.HandlerFunc{
+	return func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"hello": "found me",
+		})
+	}
 }
--- src/restapi/httpd/main.go
+++ src/restapi/httpd/main.go
@@ -7,6 +7,6 @@ import (
 )
 func main() {
 	r := gin.Default()
-	r.GET("/ping", handler.PingGet)
+	r.GET("/ping", handler.PingGet())
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
