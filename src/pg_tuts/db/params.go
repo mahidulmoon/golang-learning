e8b70942e2f48369f531e42321d6a487e0003835
mahidulmoon <mahidulmoon@gmail.com>
2020-10-04 10:52:04 +0600

    placeholder simple query with param  success

--- src/pg_tuts/db/params.go
+++ src/pg_tuts/db/params.go
@@ -5,10 +5,19 @@ import (
 	"github.com/go-pg/pg"
 )
 
+type Params struct{
+	Param1 string
+	Param2 string
+}
+
 func PlaceHolderDemo(db *pg.DB) error{
-	var value int
+	var value string
+	params := Params{
+		Param1: "this is param 1",
+		Param2: "this is param 2",
+	}
 	var query string = "Select ?"
-	_, selectErr:=db.Query(pg.Scan(&value),query,42)
+	_, selectErr:=db.Query(pg.Scan(&value),query,params)
 	if selectErr != nil{
 		fmt.Println("error while running the select query")
 		return selectErr
