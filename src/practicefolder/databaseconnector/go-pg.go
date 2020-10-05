2404964e92ce0789609e378e3357253f69a4b3c2
mahidulmoon <mahidulmoon@gmail.com>
2020-10-05 10:35:08 +0600

    database connection successfull

---
+++ src/practicefolder/databaseconnector/go-pg.go
@@ -0,0 +1,30 @@
+package databaseconnector
+
+import(
+	"fmt"
+	"os"
+	pg "github.com/go-pg/pg"
+)
+
+func Connect() *pg.DB{
+	opts := &pg.Options{
+		User: "postgres",
+		Password: "1234",
+		Addr: "localhost:5432",
+		Database: "pratice",
+	}
+	var db *pg.DB = pg.Connect(opts)
+	if db == nil{
+		fmt.Println("Failed to connect to databse ")
+		os.Exit(100)
+	}
+	fmt.Println("Connection to database successful")
+	//CreateProductItemsTable(db)
+	// closeErr := db.Close()
+	// if closeErr != nil{
+	// 	fmt.Println("Error while closing the connection")
+	// 	os.Exit(100)
+	// }
+	// fmt.Println("Connection close successfully")
+	return db
+}
---
+++ src/practicefolder/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+	//db "practicefolder/databaseconnector"
+)
+
+func main(){
+	fmt.Println("hello")
+	//db.Connect()
+}
