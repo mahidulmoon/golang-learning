eb1b389290246ead6b63b495f521f19989323cfa
mahidulmoon <mahidulmoon@gmail.com>
2020-10-06 10:09:28 +0600

    db connection successfull

---
+++ src/MovieRater/databaseconnector/go-pg.go
@@ -0,0 +1,32 @@
+package databaseconnector
+
+import(
+	"fmt"
+	"os"
+	pg "github.com/go-pg/pg"
+)
+
+var db *pg.DB
+
+func Connect() *pg.DB{
+	opts := &pg.Options{
+		User: "postgres",
+		Password: "1234",
+		Addr: "localhost:5432",
+		Database: "movierater",
+	}
+	db = pg.Connect(opts)
+	if db == nil {
+		fmt.Println("Failed to connect to databse ")
+		os.Exit(100)
+	}
+	fmt.Println("Connection to database successful")
+	//CreateProductItemsTable(db)
+	// closeErr := db.Close()
+	// if closeErr != nil{
+	// 	fmt.Println("Error while closing the connection")
+	// 	os.Exit(100)
+	// }
+	// fmt.Println("Connection close successfully")
+	return db
+}
---
+++ src/MovieRater/main.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	//"fmt"
+	db "MovieRater/databaseconnector"
+)
+
+func main(){
+	//fmt.Println("hello")
+	db.Connect()
+}
