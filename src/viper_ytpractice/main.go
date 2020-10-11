dc03dd5a1830de3011152ee3faf455e96b809bd9
mahidulmoon <mahidulmoon@gmail.com>
2020-10-11 21:40:51 +0600

    viper automated  config change

--- src/viper_ytpractice/main.go
+++ src/viper_ytpractice/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
 
@@ -19,6 +21,14 @@ var (
 		".",
 	}
 )
+
+type Config struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	Database string
+}
 
 func main() {
 	for k, v := range defaults {
@@ -36,4 +46,25 @@ func main() {
 	fmt.Println("Password from viper: ", viper.GetString("password"))
 	fmt.Println("Host from viper: ", viper.GetString("host"))
 	fmt.Println("Port from viper: ", viper.GetInt("port"))
+
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		log.Fatalf("Couldnot decode config into struct: %v", err)
+	}
+	fmt.Println("Username from struct: ", config.Username)
+	fmt.Println("Config struct: ", config)
+	changed := false
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		err = viper.Unmarshal(&config)
+		if err != nil {
+			log.Printf("could not decode config after changed : %v", err)
+		}
+		changed = true
+	})
+	for !changed {
+		time.Sleep(time.Second)
+		fmt.Println("Config struct: ", config)
+	}
 }
