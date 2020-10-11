6cfb71f5d7dd26a0dc0266105b00f81d5695d3ae
mahidulmoon <mahidulmoon@gmail.com>
2020-10-12 00:01:02 +0600

    cobra command added successfull

--- src/cobra_ytpractice/main.go
+++ src/cobra_ytpractice/main.go
@@ -2,21 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var (
-	rootCmd = &cobra.Command{
+	localRootFlag   bool
+	persistRootFlag bool
+	rootCmd         = &cobra.Command{
 		Use:   "example",
 		Short: "An example of cobra program",
 		Long: `This is a simple example of a cobra program.
 		It will have several subcommand and flags.`,
+		// Run: func(cmd *cobra.Command, args []string) {
+		// 	fmt.Println("hello from the room command")
+		// },
+	}
+	echoCmd = &cobra.Command{
+		Use:   "echo, [string to echo ]",
+		Short: "prints given string to stdout",
+		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("hello from the room command")
+			fmt.Println("Echo: " + strings.Join(args, ""))
 		},
 	}
 )
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlag", "p", false, "apersistent root flag")
+	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
+	rootCmd.AddCommand(echoCmd)
+}
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
