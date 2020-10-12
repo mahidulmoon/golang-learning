bf00ed0b300e89180588e28dd0fc99d3b596c5be
mahidulmoon <mahidulmoon@gmail.com>
2020-10-12 10:08:22 +0600

    sub command added

--- src/cobra_ytpractice/main.go
+++ src/cobra_ytpractice/main.go
@@ -10,6 +10,7 @@ import (
 var (
 	localRootFlag   bool
 	persistRootFlag bool
+	times           int
 	rootCmd         = &cobra.Command{
 		Use:   "example",
 		Short: "An example of cobra program",
@@ -20,11 +21,21 @@ var (
 		// },
 	}
 	echoCmd = &cobra.Command{
-		Use:   "echo, [string to echo ]",
+		Use:   "echo, [string to echo]",
 		Short: "prints given string to stdout",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Echo: " + strings.Join(args, ""))
+		},
+	}
+	timesCmd = &cobra.Command{
+		Use:   "times [strings to echo]",
+		Short: "prints given string to stdout multiple times",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < times; i++ {
+				fmt.Println("Echo: " + strings.Join(args, " "))
+			}
 		},
 	}
 )
@@ -32,7 +43,9 @@ var (
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlag", "p", false, "apersistent root flag")
 	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
+	timesCmd.Flags().IntVarP(&times, "times", "t", 1, "number of times to echo to stdout")
 	rootCmd.AddCommand(echoCmd)
+	echoCmd.AddCommand(timesCmd)
 }
 
 func main() {
