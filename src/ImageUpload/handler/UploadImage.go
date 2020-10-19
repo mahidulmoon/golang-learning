7b51b5ce0f17b8748fad983118dd07e6d93e1965
mahidulmoon <mahidulmoon@gmail.com>
2020-10-19 09:52:54 +0600

    imageupload successfull

--- src/ImageUpload/handler/UploadImage.go
+++ src/ImageUpload/handler/UploadImage.go
@@ -1,11 +1,38 @@
 package handler
 
 import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Upload() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, "Bad request")
+			return
+		}
+
+		filename := header.Filename
+
+		fmt.Println(file, err, filename)
+
+		out, err := os.Create("static/" + filename)
+		//Note that the static/uploadfile / here is not / static/uploadfile/
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		c.JSON(200, gin.H{
 			"hello": "success",
 		})
