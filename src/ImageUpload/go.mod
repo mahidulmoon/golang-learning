2d57038e7807d4fcbc13610568e9296bdd628585
mahidulmoon <mahidulmoon@gmail.com>
2020-10-19 10:28:32 +0600

    image compress and resize success

--- src/ImageUpload/go.mod
+++ src/ImageUpload/go.mod
@@ -2,4 +2,7 @@ module ImageUpload
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
--- src/ImageUpload/handler/UploadImage.go
+++ src/ImageUpload/handler/UploadImage.go
@@ -2,12 +2,13 @@ package handler
 
 import (
 	"fmt"
-	"io"
+	"image/jpeg"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nfnt/resize"
 )
 
 func Upload() gin.HandlerFunc {
@@ -22,16 +23,24 @@ func Upload() gin.HandlerFunc {
 
 		fmt.Println(file, err, filename)
 
+		img, err := jpeg.Decode(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		m := resize.Resize(795, 636, img, resize.Lanczos3)
+
 		out, err := os.Create("static/" + filename)
 		//Note that the static/uploadfile / here is not / static/uploadfile/
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer out.Close()
-		_, err = io.Copy(out, file)
-		if err != nil {
-			log.Fatal(err)
-		}
+		// _, err = io.Copy(out, file)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		jpeg.Encode(out, m, nil)
 
 		c.JSON(200, gin.H{
 			"hello": "success",
