b91a0a3206df294a022ef00f98a31daaf6e4abf2
mahidulmoon <mahidulmoon@gmail.com>
2020-11-18 11:09:46 +0600

    get/post through onterface

--- src/youtube_crash/server.go
+++ src/youtube_crash/server.go
@@ -1,14 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"youtube_crash/controller"
+	"youtube_crash/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "hellow",
-		})
+	// server.GET("/test", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{
+	// 		"message": "hellow",
+	// 	})
+	// })
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/post", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 
 	server.Run(":8080")
