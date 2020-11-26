57c890b638c04d83891010dd6d3f0389994ede46
mahidulmoon <mahidulmoon@gmail.com>
2020-11-26 16:24:52 +0600

    varibales

---
+++ src/udemy basic/cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Titanic" //scope variable
+
+	card := "Rise of Empire"
+	card = "Using old variable" //if variable is declared before than no need to give : befor =
+
+	fmt.Println(card)
+}
