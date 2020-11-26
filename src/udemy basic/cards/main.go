5178aa39aa94ba15d2830ebefda269de540c4c35
mahidulmoon <mahidulmoon@gmail.com>
2020-11-26 16:53:56 +0600

    getting value from func

--- src/udemy basic/cards/main.go
+++ src/udemy basic/cards/main.go
@@ -9,4 +9,11 @@ func main() {
 	card = "Using old variable" //if variable is declared before than no need to give : befor =
 
 	fmt.Println(card)
+
+	card_from_func := newCard()
+	fmt.Println(card_from_func)
+}
+
+func newCard() string {
+	return "Game of thrones"
 }
