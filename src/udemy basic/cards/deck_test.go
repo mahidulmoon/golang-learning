8da7c932f04174abedcef81bcc2bbf3760b0aeac
mahidulmoon <mahidulmoon@gmail.com>
2020-11-30 18:46:16 +0600

    testing with file input output

--- src/udemy basic/cards/deck_test.go
+++ src/udemy basic/cards/deck_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-
+	"os"
 	"testing"
 )
 
@@ -11,4 +11,20 @@ func newDeckTest(t *testing.T) {
 	if len(newdeck) != 16 {
 		t.Errorf("Expected deck length of 16 but got %v",len(newdeck))
 	}
+}
+
+func TestDecktosaveandTakeNewDeckFromFile (t *testing.T){
+	os.Remove("_decTesting")
+
+	deck := newDeck()
+
+	deck.savetoFile("_decTesting")
+
+	loadDeck := newDecFromFile("_decTesting")
+
+	if len(loadDeck) != 16{
+		t.Errorf("Expecting deck length is 16 but got %v",len(loadDeck))
+	}
+
+	os.Remove("_decTesting")
 }
