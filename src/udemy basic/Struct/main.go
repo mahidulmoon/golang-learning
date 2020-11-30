fadd06ee8e546ae4bb56a6a6cdeed0b55febb420
mahidulmoon <mahidulmoon@gmail.com>
2020-11-30 19:12:34 +0600

    nested struct added

--- src/udemy basic/Struct/main.go
+++ src/udemy basic/Struct/main.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
+type contactInfo struct{
+	email string
+	zip int
+}
+
 type person struct{
 	firstname string
 	lastname string
+	contact contactInfo
 }
 
+
 func main()  {
-	name := person{firstname: "mahidul",lastname: "moon"}
+	name := person{firstname: "mahidul",lastname: "moon",contact:contactInfo{email:"mahidulmoon@gmail.com",zip:2216},}
 	fmt.Println(name)
 
 	var updatename person
