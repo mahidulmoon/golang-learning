b8bccf4f1466821f9a0eaf3a444f52bde6822a39
mahidulmoon <mahidulmoon@gmail.com>
2020-12-05 09:36:28 +0600

    implementing channels

--- src/udemy basic/channels/main.go
+++ src/udemy basic/channels/main.go
@@ -12,18 +12,23 @@ func main(){
 		"http://stackoverflow.com",
 		"http://golang.org",
 	}
+	c := make(chan string)
 
 	for _,link := range links{
-		responseCheck(link)
+		responseCheck(link,c)
 		//go responseCheck(link) // go only use before function calling and it is not care about waiting for child function response just execute
 	}
+	fmt.Println(<-c)
 }
 
 
-func responseCheck(link string){
+func responseCheck(link string,c chan string){
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link,"Might be down")
+		c <- "Might be down I think"
 	}
 	fmt.Println(link,"server is ok")
+	c <- "Yes It is fine"
+
 }
