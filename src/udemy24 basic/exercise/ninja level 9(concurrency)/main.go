9971463b1dc5469177e2534ceea44544014a2597
mahidulmoon <mahidulmoon@gmail.com>
2020-12-09 18:01:57 +0600

    readin cpu and goroutine inside anonymus function

--- src/udemy24 basic/exercise/ninja level 9(concurrency)/main.go
+++ src/udemy24 basic/exercise/ninja level 9(concurrency)/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 func main(){
+	fmt.Println("Begin CPU:",runtime.NumCPU())
+	fmt.Println("Begin GS:",runtime.NumGoroutine())
 
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -18,6 +21,12 @@ func main(){
 		wg.Done()
 	}()
 
+	fmt.Println("Mid CPU:",runtime.NumCPU())
+	fmt.Println("Mid GS:",runtime.NumGoroutine())
+
 	wg.Wait()
 	fmt.Println("Main run successfully")
+
+	fmt.Println("END CPU:",runtime.NumCPU())
+	fmt.Println("END GS:",runtime.NumGoroutine())
 }
