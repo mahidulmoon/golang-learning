5c86c1d18cf11fbcb8c83c38ff07b686f3b11b5a
mahidulmoon <mahidulmoon@gmail.com>
2020-12-12 11:20:04 +0600

    activating channel value with buffer solution

--- src/udemy24 basic/exercise/ninja level 10/main.go
+++ src/udemy24 basic/exercise/ninja level 10/main.go
@@ -12,4 +12,10 @@ func main(){
 
 
 	fmt.Println(<-c) //going to block the channel for a while
+
+	//working with buffer solution
+
+	d:= make(chan int,1)	//this is going to say that chan attribute quantity
+	d <- 219
+	fmt.Println(d)
 }
