635f0353f8467fd9e20c14646bf80dce83deaa3c
mahidulmoon <mahidulmoon@gmail.com>
2020-12-12 12:15:18 +0600

    simple chan with for loop

---
+++ src/udemy24 basic/exercise/ninja level 10/simple_chan_understand.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	c := make(chan int)
+
+	go func() {
+		for i:=0;i<10;i++{
+			c <- i
+		}
+		close(c)
+	}()
+
+	for v := range c{
+		fmt.Println(v)
+	}
+	fmt.Println("Now going to end")
+}
