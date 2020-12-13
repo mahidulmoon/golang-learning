56bbab60758465bcb26f89eb7797ec85584fba4c
mahidulmoon <mahidulmoon@gmail.com>
2020-12-13 11:49:29 +0600

    added testing

--- src/udemy24 basic/exercise/ninja level 12 writing documentation/dog/dog_test.go
+++ src/udemy24 basic/exercise/ninja level 12 writing documentation/dog/dog_test.go
@@ -1,9 +1,16 @@
 package dog
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func BenchmarkYears(b *testing.B) {
 	for i:=0;i<b.N;i++{
 		Years(i)
 	}
+}
+
+func YearTest(t *testing.T){
+	fmt.Println(Years(10))
 }
