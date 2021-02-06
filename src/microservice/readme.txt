1759b631f3fa76f4f6199b6f1389e46c22bf71ca
mahidulmoon <mahidulmoon@gmail.com>
2021-02-06 15:13:39 +0600

    passing data into  microservice with go

--- src/microservice/readme.txt
+++ src/microservice/readme.txt
@@ -1,3 +1,5 @@
 # run: go run main.go
 
 # to work a microservice :(in another terminal)  curl -v localhost:port/url
+
+#to push data in a microservice: curl -v -d data localshot:port
--- src/microservice/working/main.go
+++ src/microservice/working/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main(){
-	http.HandleFunc("/",func(http.ResponseWriter,*http.Request){
+	http.HandleFunc("/",func(rw http.ResponseWriter,r*http.Request){
 		log.Println("Hello world")
+		d,_ := ioutil.ReadAll(r.Body)
+		log.Printf("Data %s",d)
 	})
 	http.HandleFunc("/goodbye",func(http.ResponseWriter,*http.Request){
 		log.Println("Good Bye world")
