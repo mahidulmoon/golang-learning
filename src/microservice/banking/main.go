f257f3cc3ea11a6bf450386d2c01a3f5127f0999
mahidulmoon <mahidulmoon@gmail.com>
2021-03-06 13:16:47 +0600

    setting up gorillamux

--- src/microservice/banking/main.go
+++ src/microservice/banking/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/mahidulmoon/banking/handler"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main(){
 
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	mux := mux.NewRouter()
 
 	mux.HandleFunc("/greet", handler.Greet)
 	mux.HandleFunc("/customers",handler.GetAllCustomer)
